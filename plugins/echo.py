51edcc4e39ad38d0b444af43c200c7865108b9b1
Paul Tolstoi <paul.tolstoi@stustanet.de>
2016-04-13 18:10:51 +0200

    added broadcast to all connected channels

--- plugins/echo.py
+++ plugins/echo.py
@@ -4,3 +4,8 @@ def register(bot):
         bot.send_message(match.group(1), msg['from'].bare)
 
     bot.respond('echo (.*)$', echo)
+
+    def broadcast(bot, msg, match):
+        bot.send_message(match.group(1))
+
+    bot.respond('broadcast (.*)$', broadcast)
